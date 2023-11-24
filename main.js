ae4a7a1d74062ce3f9830e81455ca5349ea2f987
Tahomid op <ffconfig.op@gmail.com>
2023-11-24 14:41:07 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,54 +1,41 @@
-// authenticates you with the API standard library
-const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
+const { Client, GatewayIntentBits } = require('discord.js');
 
-let members;
-try { 
-  members = await lib.discord.guilds['@0.2.3'].members.list({
-    guild_id: `${context.params.event.guild_id}`,
-    limit: 1000
-  });
-} catch (e) {
-  await lib.discord.channels['@0.3.1'].messages.create({
-    channel_id: `${context.params.event.channel_id}`,
-    content: [
-      `Whoops, it seems retrieving the number of members in the guild failed.`,
-      `Your bot needs special permission to view the members in your guild.`,
-      '',
-      `You can check out the README for this Autocode app for help setting that up:`,
-      'https://autocode.com/app/discord/basic-discord-example/'
-    ].join('\n')
-  });
-  throw e;
-}
+// Set your bot token from the Discord Developer Portal
+const TOKEN = 'YOUR_BOT_TOKEN';
 
-let memberCountText = members.length < 1000 ? members.length : 'over 1000';
+// Set your prefix
+const PREFIX = '?';
 
-let messageResponse = await lib.discord.channels['@0.3.1'].messages.create({
-  channel_id: `${context.params.event.channel_id}`,
-  content: [
-    `Hey <@!${context.params.event.member.user.id}>! I'm a bot powered by Autocode.`,
-    `There are currently **${memberCountText}** users and bots in this guild.`
-  ].join('\n'),
-  embed: {
-    title: 'Guild Information',
-    type: 'rich',
-    color: 0x0000AA, // Blue color
-    description: 'You could add some information here for guild members to view!',
-    fields: [{
-      name: 'Message Formatting',
-      value: [
-        'Check out this link for more details on formatting message embeds:',
-        'https://discord.com/developers/docs/resources/channel#embed-object-embed-structure'
-      ].join('\n')
-    }, {
-      name: 'Setting up Slash Commands',
-      value: [
-        'Check out the README for this bot on Autocode for help setting up slash commands:',
-        'https://autocode.com/app/discord/basic-discord-example/'
-      ].join('\n')
-    }]
-  },
-  tts: false
+// Create a new Discord client
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+  ],
 });
 
-return messageResponse;
+// Event: Bot is ready
+client.once('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+// Event: Message received
+client.on('messageCreate', (message) => {
+  if (message.content.startsWith(PREFIX) && !message.author.bot) {
+    const args = message.content.slice(PREFIX.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    // Command: Display server stats
+    if (command === 'serverstats') {
+      const server = message.guild;
+      const members = server.memberCount;
+      const onlineMembers = server.members.cache.filter((member) => member.presence.status === 'online').size;
+      message.channel.send(`Server Name: ${server.name}\nMembers: ${members}\nOnline Members: ${onlineMembers}`);
+    }
+  }
+});
+
+// Log in to Discord with your app's token
+client.login(TOKEN);
+￼Enter
